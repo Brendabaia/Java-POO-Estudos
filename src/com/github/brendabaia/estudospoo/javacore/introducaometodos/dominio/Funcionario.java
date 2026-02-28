0076524d7c5706eee1d52158a943d3c89d8a9967
Brenda <br3nd4lima@gmail.com>
2026-02-27 23:43:38 -0300

    Orientação Objetos - Métodos - Exercise

---
+++ src/com/github/brendabaia/estudospoo/javacore/introducaometodos/dominio/Funcionario.java
@@ -0,0 +1,26 @@
+package com.github.brendabaia.estudospoo.javacore.introducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public float salarios [];
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        for (double salario: salarios){
+            System.out.print(salario + " ");
+        }
+        imprimeMediaSalario();
+    }
+
+    public void imprimeMediaSalario(){
+        double media = 0;
+        for (double salario: salarios){
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("\nMedia salarial " + media);
+    }
+
+}
---
+++ src/com/github/brendabaia/estudospoo/javacore/introducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,16 @@
+package com.github.brendabaia.estudospoo.javacore.introducaometodos.test;
+
+import com.github.brendabaia.estudospoo.javacore.introducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario =  new Funcionario();
+
+        funcionario.nome = "Brenda";
+        funcionario.idade = 24;
+        funcionario.salarios = new float[] {1200, 987.32f, 2000};
+
+        funcionario.imprime();
+        funcionario.imprimeMediaSalario();
+    }
+}
